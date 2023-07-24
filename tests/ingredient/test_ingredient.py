c0cd743c8a05389b833e23eb57d4750da05c514b
NettoJM <nettojm86@gmail.com>
2023-07-24 18:33:06 -0300

    Add tests for Restriction and Ingredient classes

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,27 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    mussarela = Ingredient("queijo mussarela")
+    farinha = Ingredient("farinha")
+
+    # Test for equality (__eq__)
+    assert mussarela == mussarela
+    assert mussarela != farinha
+
+    # Test for hash (__hash__)
+    assert hash(mussarela) == hash(mussarela)
+    assert hash(mussarela) != hash(farinha)
+
+    # Test for repr (__repr__)
+    assert repr(mussarela) == "Ingredient('queijo mussarela')"
+
+    # Test for name attribute
+    assert mussarela.name == "queijo mussarela"
+
+    # Test for restrictions attribute
+    assert mussarela.restrictions == {
+        Restriction.LACTOSE,
+        Restriction.ANIMAL_DERIVED,
+    }
